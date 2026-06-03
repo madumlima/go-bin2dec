4da80d1d14eca33632d1b61152084fbd64746e33
Maria Eduarda <medeiroslima.me@gmail.com>
2026-06-03 13:49:40 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-bin-to-dec
+
+go 1.23
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Enter the binary number (up to 8 digits): ")
+
+	var input string
+	_, err := fmt.Scan(&input)
+	if err != nil {
+		fmt.Println("Error reading input")
+		return
+	}
+
+	var dec int
+
+	for i, number := range input {
+		if number != '0' && number != '1' {
+			err = errors.New("only 0s and 1s are allowed")
+			fmt.Println(err)
+			return
+
+		}
+		var byteChar = byte(number) - '0'
+
+		exponent := len(input) - 1 - i
+		dec = dec + (int(byteChar) * int(math.Pow(2, float64(exponent))))
+	}
+
+	fmt.Println(dec)
+}
