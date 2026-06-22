89a70aa228d6511bc9a5686635f01402206c7838
madumlima <medeiroslima.me@gmail.com>
2026-06-22 12:11:43 -0300

    refactor: use interface for the Converter type

--- main.go
+++ main.go
@@ -6,6 +6,31 @@ import (
 	"math"
 )
 
+type Converter interface {
+	Convert(input string) (int, error)
+}
+
+type BinaryConverter struct{}
+
+func (b BinaryConverter) Convert(input string) (int, error) {
+	var decimal int
+
+	var err error
+	for i, number := range input {
+		if number != '0' && number != '1' {
+			err = errors.New("only 0s and 1s are allowed")
+			fmt.Println(err)
+			return 0, err
+		}
+		var byteChar = byte(number) - '0'
+
+		exponent := len(input) - 1 - i
+		decimal = decimal + (int(byteChar) * int(math.Pow(2, float64(exponent))))
+
+	}
+	return decimal, err
+}
+
 func main() {
 	fmt.Println("Enter the binary number (up to 8 digits): ")
 
@@ -22,20 +47,11 @@ func main() {
 		return
 	}
 
-	var dec int
-
-	for i, number := range input {
-		if number != '0' && number != '1' {
-			err = errors.New("only 0s and 1s are allowed")
-			fmt.Println(err)
-			return
-
-		}
-		var byteChar = byte(number) - '0'
-
-		exponent := len(input) - 1 - i
-		dec = dec + (int(byteChar) * int(math.Pow(2, float64(exponent))))
+	var binary Converter = BinaryConverter{}
+	bin, err := binary.Convert(input)
+	if err != nil {
+		return
 	}
+	fmt.Println(bin)
 
-	fmt.Println(dec)
 }
